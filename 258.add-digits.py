7a8e3c3ca904f7f5470d146687f52363c0e2bd88
jiangyunfeng <jiangyunfeng@wepiao.com>
2016-07-18 22:57:13 +0800

    Fix pep8 problem

--- 258.add-digits.py
+++ 258.add-digits.py
@@ -3,16 +3,17 @@
 
 class Solution(object):
     '''
-    Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
-    
+    Given a non-negative integer num, repeatedly add all
+        its digits until the result has only one digit.
+
     For example:
-    
-    Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
-    
+
+    Given num = 38, the process is like: 3 + 8 = 11,
+        1 + 1 = 2. Since 2 has only one digit, return it.
+
     TODO
     Follow up:
     Could you do it without any loop/recursion in O(1) runtime?
-    
     '''
     def addDigits(self, num):
         """
@@ -20,7 +21,7 @@ class Solution(object):
         :rtype: int
         """
         s = sum([int(i) for i in str(num)])
-        if s>= 10:
+        if s >= 10:
             return self.addDigits(s)
         return s
 
